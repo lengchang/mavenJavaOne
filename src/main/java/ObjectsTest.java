6096b7db4b987a6a4a437c687cafbeeb37ad6424
SKY-20170916TCP\Administrator <1006519039@qq.com>
2017-10-11 08:28:52 +0800

    MavenJavaOne

--- src/main/java/ObjectsTest.java
+++ src/main/java/ObjectsTest.java
@@ -1,5 +1,16 @@
+import java.util.Objects;
+
 /**
  * Created by Administrator on 2017/10/11 0011.
  */
 public class ObjectsTest {
+    static ObjectsTest obj;
+    public static void main(String[] args)
+    {
+        System.out.println(Objects.hashCode(obj));
+        System.out.println(Objects.toString(obj));
+        System.out.println(Objects.requireNonNull(obj));
+
+
+    }
 }
